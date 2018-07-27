cf2663bddba60eba35289ff9701179cfc954d3f1
Dave Wheeler <dwheelerau@gamil.com>
2018-07-27 20:55:32 +1000

    working zoom

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,11 @@
+// HEAVILY inspired (AKA copied from
+// https://raw.githubusercontent.com/alignedleft/d3-book/master/chapter_14/17_labels.html
+// From the great book: Interactive Data Visualization for the web. 
 var h = 500;
 var w = 500;
 
 var projection = d3.geoMercator()
-    .center([130, -30])
-    .scale([500])
+    .center([130, -20])
     .translate([w/2, h/2]);
 
 var path = d3.geoPath()
@@ -14,9 +16,64 @@ var svg = d3.select("#map")
     .attr("height", h)
     .attr("width", w);
 
+//Define what to do when panning or zooming
+var zooming = function(d) {
+    //console.log(d3.event.transform);
+
+    //New offset array
+    var offset = [d3.event.transform.x, d3.event.transform.y];
+    console.log(offset);
+
+    //Calculate new scale was 2000
+    var newScale = d3.event.transform.k * 2000;
+
+    //Update projection with new offset and scale
+    projection.translate(offset)
+        .scale(newScale);
+
+    //Update all paths and circles
+    svg.selectAll("path")
+        .attr("d", path);
+
+    svg.selectAll("circle")
+        .attr("cx", function(d) {
+            return projection([d.lon, d.lat])[0];
+        })
+        .attr("cy", function(d) {
+            return projection([d.lon, d.lat])[1];
+        });
+}
+
+//Then define the zoom behavior, translateExtent controls limits
+var zoom = d3.zoom()
+    .scaleExtent([ 0.2, 2.0 ])
+    .translateExtent([[ -900, -1000 ], [ 900, 1000 ]])
+    .on("zoom", zooming);
+
+//The center of the country, roughly
+//var center = projection([130, -30]);
+var center = projection([130, -30]);
+
+//Create a container in which all zoom-able elements will live
+var map = svg.append("g")
+    .attr("id", "map")
+    .call(zoom)  //Bind the zoom behavior
+    .call(zoom.transform, d3.zoomIdentity  //Then apply the initial transform
+    .translate(w/2, h/2)
+    .scale(0.25)
+    .translate(-center[0], -center[1]));
+
+//Create a new, invisible background rect to catch zoom events
+map.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("opacity", 0);
+
 // load the pub dataO
 d3.json("static/data/aust.json", function(json) {
-    svg.selectAll("path")
+    map.selectAll("path")
         .data(json.features)
         .enter()
         .append("path")
@@ -26,8 +83,7 @@ d3.json("static/data/aust.json", function(json) {
 
 
     d3.csv("static/data/data_filt2.csv", function(data) {
-        console.log(data);
-        svg.selectAll("circle")
+        map.selectAll("circle")
             .data(data)
             .enter()
             .append("circle")
@@ -37,37 +93,17 @@ d3.json("static/data/aust.json", function(json) {
             .attr("cy", function(d) {
                 return projection([d.lon, d.lat])[1];
             })
-            .attr("r", 0.9)
-            .style("fill", "red")
+            .attr("r", 2)
+            .style("fill", "orange")
             .append("title")
             .text(function(d) {
-                console.log(d.sub_info);
                 return d.name + " " + d.sub_info;
             });
-            //.on("mouseover", mouseover)
-            //.on("mouseout", mouseout);
     createZoomButtons();
     });
 
-    function mouseover(d) {
-        d3.select("#tooltip")
-            .select("value")
-            .text("test");
-        d3.select("#tooltip").classed("hidden", false);
-        d3.select("#tootlip")
-            .style("top", (event.pageY-10) + "px")
-            .style("left", (event.pageX-10) + "px");
-    }
-
-    function mouseout(d) {
-        d3.select("#tooltip").classed("hidden", "true");
-    }
-
-    //Create zoom buttons
     var createZoomButtons = function() {
-        //Create the clickable groups
-
-        //Zoom in button
+        // Zoom in button shapes
         var zoomIn = svg.append("g")
             .attr("class", "zoom")	//All share the 'zoom' class
             .attr("id", "in")		//The ID will tell us which direction to head
@@ -84,7 +120,6 @@ d3.json("static/data/aust.json", function(json) {
             .attr("y", 20)
             .text("+");
 
-        //Zoom out button
         var zoomOut = svg.append("g")
             .attr("class", "zoom")
             .attr("id", "out")
@@ -101,28 +136,26 @@ d3.json("static/data/aust.json", function(json) {
             .attr("y", 20)
             .html("&ndash;");
 
-        //Zooming interaction
-
+        // Zooming action itself
         d3.selectAll(".zoom")
             .on("click", function() {
-            console.log('testtsts');
-        //Set how much to scale on each click
-        var scaleFactor;
+                //Set how much to scale on each click
+                var scaleFactor;
 
-        //Which way are we headed?
-        var direction = d3.select(this).attr("id");
+                //Which way are we headed?
+                var direction = d3.select(this).attr("id");
 
-        //Modify the k scale value, depending on the direction
-        switch (direction) {
-            case "in":
-                scaleFactor = 1.5;
-                break;
-            case "out":
-                scaleFactor = 0.75;
-                break;
-            default:
-                break;
-        }
+                //Modify the k scale value, depending on the direction
+                switch (direction) {
+                    case "in":
+                        scaleFactor = 1.5;
+                        break;
+                    case "out":
+                        scaleFactor = 0.75;
+                        break;
+                    default:
+                        break;
+            }
 
         //This triggers a zoom event, scaling by 'scaleFactor'
         map.transition()
@@ -132,3 +165,15 @@ d3.json("static/data/aust.json", function(json) {
 });
 
 
+//Bind 'Reset' button behavior
+d3.select("#reset")
+    .on("click", function() {
+        map.transition()
+            .call(zoom.transform,
+        d3.zoomIdentity
+        //Same
+            .translate(w/2, h/2)
+            .scale(0.25)
+            .translate(-center[0], -center[1])
+            );
+});
